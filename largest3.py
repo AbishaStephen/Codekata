9bcec348e1d5ac22b7811c5111f79ef5ea457bac
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-06-21 11:39:28 +0530

    Create largest3.py

---
+++ largest3.py
@@ -0,0 +1,9 @@
+a=input("Enter the value for a")
+    b=input("Enter the value for b")
+    c=input("Enter the value for c")
+    if(a>b and a>c):
+        print ("a is maximum")
+    elif(b>a and b>c):
+        print ("b is maximum")
+    else:
+        print ("c is maximum")
