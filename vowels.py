af7453c82a6d8efeeaadaa2c3819f3d2ccf2c75d
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-06-21 11:31:23 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,5 @@
+str=input("enter the string")
+    if(str.isalpha()=='true'):
+        print("The given string has characters ")
+    else:
+        print("The given string does not have characters ")
