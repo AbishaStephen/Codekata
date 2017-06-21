3338d160f7fec58b1f005c1f6f1517c4a53e8471
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-06-21 12:04:36 +0530

    Create posorneg.py

---
+++ posorneg.py
@@ -0,0 +1,7 @@
+m=int(input("Enter the Number"))
+  if(m>0):
+    print("Entered number is positive")
+  elif(m<0):
+    print("Entered number is negative")
+  else:
+    print("Entered number is 0")
