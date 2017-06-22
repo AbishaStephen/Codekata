e8970db7b695f4d32319e783d0c4f13b983407a6
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-06-22 21:30:16 +0530

    Create interest.py

---
+++ interest.py
@@ -0,0 +1,5 @@
+p=int(input("Enter the principal value"))
+t=int(input("Enter the number of years"))
+r=int(input("Enter the rate of interest"))
+interest=(p*t*r)/100
+print("The simple interest is:",interest)
