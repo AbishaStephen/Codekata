ebfe55cfda921a03de3c0df94900c8f8b0816363
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-06-23 10:27:50 +0530

    Create day.py

---
+++ day.py
@@ -0,0 +1,7 @@
+ s=input("Enter the day")
+    s1=['MONDAY','TUESDAY','WEDNESDAY','THURSDAY','FRIDAY']
+    s2=['SATURDAY','SUNDAY']
+    if(s in s1):
+         print("Workingday")
+    if(s in s2):
+        print("Holiday")
