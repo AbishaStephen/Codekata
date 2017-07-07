4e3c626c8eb2c56624b532fcbc86ef7fec7ae35d
Abisha Stephen S.P <abiabisha692@gmail.com>
2017-07-07 14:14:20 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,5 @@
+j=input("Enter the letter:  ")
+i=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for k in range(len(i)):
+  if j in i[k]:
+      print k+1
